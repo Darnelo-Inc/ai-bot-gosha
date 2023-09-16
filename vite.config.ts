2bc56f76a590522d091c0d6e3a1cd6c850b2b8ac
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-16 19:26:46 +0300

    deploy configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ai-bot-gosha/",
   build: {
-    outDir: "build",
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
 })
